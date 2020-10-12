6fa59e5a898effcb053d7f833091aa334f0f3fa1
kamalsroor <teko.2013@gmail.com>
2020-10-12 18:44:47 +0200

    fix

--- app/Http/Controllers/Admin/Settings.php
+++ app/Http/Controllers/Admin/Settings.php
@@ -31,7 +31,7 @@ class Settings extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function store(Request $request) {
-	
+	dd($request->all());
 		$rules = [
 			// 'sitename_ar'    => 'required',
 			'sitename_en'    => 'required',
@@ -119,7 +119,7 @@ class Settings extends Controller {
 		
 		if (request()->hasFile('about_video')) {
 			$data['about_video'] = it()->upload('about_video', 'setting');
-			dd($data['about_video']);
+			
 		}
 		if (request()->hasFile('product_video')) {
 			$data['product_video'] = it()->upload('product_video', 'setting');
